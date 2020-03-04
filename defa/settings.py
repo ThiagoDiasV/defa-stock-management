66f7493c224aeca853c66a2f3966a0d73b64c8b6
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-03 21:57:37 -0300

    heroku ready stock part

--- defa/settings.py
+++ defa/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
