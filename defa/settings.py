bea54232d3c1f65d9eac104da62595ca6e9509fd
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-04 08:20:34 -0300

    heroku ready stock part

--- defa/settings.py
+++ defa/settings.py
@@ -119,4 +119,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 # Configure Postgres for Heroku
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
