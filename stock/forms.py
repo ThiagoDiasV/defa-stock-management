de55cf883b015dfeb1aeb5857b8389c3f80a5066
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-04 09:47:39 -0300

    trying to make changes on stock.forms

--- stock/forms.py
+++ stock/forms.py
@@ -5,12 +5,12 @@ from django.db.utils import ProgrammingError
 
 class StockCategoryForm(forms.Form):
     try:
-        queryset = Stock.objects.values_list("category", flat=True).distinct()
-        queryset_choices = [("Todos", "Listar tudo")] + [(id, id) for id in queryset]
-        category = forms.ChoiceField(
-            label="Categoria", choices=queryset_choices, widget=forms.Select()
-        )
-
+        # queryset = Stock.objects.values_list("category", flat=True).distinct()
+        # queryset_choices = [("Todos", "Listar tudo")] + [(id, id) for id in queryset]
+        # category = forms.ChoiceField(
+        #     label="Categoria", choices=queryset_choices, widget=forms.Select()
+        # )
+        category = forms.ModelChoiceField(queryset=Stock.objects.values_list('category', flat=True).distinct())
     except ProgrammingError:
         category = forms.ChoiceField(
             label="Categoria", widget=forms.Select()
