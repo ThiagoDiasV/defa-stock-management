58ae19484e72d65a2eafea15ab66f2a6d0e053a6
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-04 08:14:55 -0300

    heroku ready stock part

--- defa/settings.py
+++ defa/settings.py
@@ -67,16 +67,16 @@ WSGI_APPLICATION = "defa.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "defa",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "defa",
+#         "USER": "postgres",
+#         "PASSWORD": "123456",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
