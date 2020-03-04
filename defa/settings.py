082bd0ff61ec0fdfc3af14cd3b080090b6b95401
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-03 21:38:29 -0300

    heroku ready stock part

--- defa/settings.py
+++ defa/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "j_7mub=(sh4eu&&e7z+@lwe20+x_bcvx!7vh4+aly2zmhmel59"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
