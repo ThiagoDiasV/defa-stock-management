08c05ded0b622d41e91ce970779d1960fbf13f19
ThiagoDiasV <thiago76ers@gmail.com>
2020-03-12 15:08:04 -0300

    fixing bug of quantity 0 on stocks queries

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render
 from .forms import StockCategoryForm
 from .models import Stock
 from django.core import serializers
@@ -10,16 +10,11 @@ def stock(request):
     return render(request, "stock/stock.html", {"form": form})
 
 
-def query_category(category):
-    queryset = Stock.objects.filter(category=category)
-    return queryset
-
-
 def category(request):
     category = request.GET["category"]
     if category == "Listar tudo":
-        queryset = Stock.objects.all()
+        queryset = Stock.objects.filter(quantity__gt=0)
     else:
-        queryset = query_category(category)
+        queryset = Stock.objects.filter(category=category, quantity__gt=0)
     json_data = serializers.serialize("json", queryset)
     return HttpResponse(json_data)
